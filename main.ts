fd0d85f3f7eb9f2974c5ddc314791d85fb21de83
gosuhiman <gosuhiman@gmail.com>
2018-12-11 03:15:49 +0100

    basic prototype

--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
-import {createClient as createTumblrClient, TumblrClient} from "tumblr.js";
-import {bindCallback} from 'rxjs'
-import {consumerKey, consumerSecret} from "./secrets/keys";
+import {tap} from "rxjs/operators";
+import {createClient as createTumblrClient, TumblrClient, TumblrClientCallback} from 'tumblr.js';
+import {bindNodeCallback, Observable} from 'rxjs'
+import {consumerKey, consumerSecret} from './secrets/keys';
+import request from 'request';
+import fs from 'fs';
+import path from 'path';
 
 const client: TumblrClient = createTumblrClient({
   credentials: {
@@ -10,9 +14,33 @@ const client: TumblrClient = createTumblrClient({
   returnPromises: true
 });
 
-const blogPosts = bindCallback(client.blogPosts);
+type blogPostsFnType = (blogIdentifier: string, type: string, params: any, callback: TumblrClientCallback) => void;
+const blogPostsFn: blogPostsFnType = client.blogPosts;
+const blogPosts = bindNodeCallback(blogPostsFn);
 
-blogPosts('friendly-animals')
-  .subscribe(posts => {
-    console.log(posts);
+function downloadBlog(blogName: string): Observable<any> {
+  return blogPosts(blogName, 'photo', {offset: 320}).pipe(tap((response: any) => {
+    response.posts.forEach((post => {
+      if (post.photos) {
+        const blogPhotosPath = path.join('download', post.blog.name);
+        if (!fs.existsSync(blogPhotosPath)) fs.mkdirSync(blogPhotosPath);
+        post.photos.forEach((photo => {
+          const sourceUrl = photo.original_size.url;
+          const fileName = path.basename(sourceUrl);
+          const photoPath = path.join(blogPhotosPath, fileName);
+          request(sourceUrl).pipe(fs.createWriteStream(photoPath));
+        }));
+      }
+    }));
+  }));
+}
+
+downloadBlog('friendly-animals')
+  .subscribe({
+    next: () => {
+      console.log('Job finished');
+    },
+    error: (err) => {
+      console.error(err);
+    },
   });
