68746340f64e6392e839ba930fddde0b8a79be80
gosuhiman <gosuhiman@gmail.com>
2018-12-11 02:03:24 +0100

    init

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-console.log('running');
+import {createClient as createTumblrClient, TumblrClient} from "tumblr.js";
+import {bindCallback} from 'rxjs'
+import {consumerKey, consumerSecret} from "./secrets/keys";
+
+const client: TumblrClient = createTumblrClient({
+  credentials: {
+    consumer_key: consumerKey,
+    consumer_secret: consumerSecret,
+  },
+  returnPromises: true
+});
+
+const blogPosts = bindCallback(client.blogPosts);
+
+blogPosts('friendly-animals')
+  .subscribe(posts => {
+    console.log(posts);
+  });
