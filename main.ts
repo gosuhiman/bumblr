b038145772d138a97f99df1fa6cff31e7711e203
gosuhiman <gosuhiman@gmail.com>
2018-12-11 03:39:32 +0100

    basic prototype

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
-import {tap} from "rxjs/operators";
-import {createClient as createTumblrClient, TumblrClient, TumblrClientCallback} from 'tumblr.js';
-import {bindNodeCallback, Observable} from 'rxjs'
-import {consumerKey, consumerSecret} from './secrets/keys';
-import request from 'request';
 import fs from 'fs';
 import path from 'path';
+import request from 'request';
+import {bindNodeCallback, Observable, of} from 'rxjs'
+import {delay, map, switchMap} from "rxjs/operators";
+import {createClient as createTumblrClient, TumblrClient, TumblrClientCallback} from 'tumblr.js';
+import {consumerKey, consumerSecret} from './secrets/keys';
 
 const client: TumblrClient = createTumblrClient({
   credentials: {
@@ -18,24 +18,41 @@ type blogPostsFnType = (blogIdentifier: string, type: string, params: any, callb
 const blogPostsFn: blogPostsFnType = client.blogPosts;
 const blogPosts = bindNodeCallback(blogPostsFn);
 
-function downloadBlog(blogName: string): Observable<any> {
-  return blogPosts(blogName, 'photo', {offset: 320}).pipe(tap((response: any) => {
-    response.posts.forEach((post => {
-      if (post.photos) {
-        const blogPhotosPath = path.join('download', post.blog.name);
-        if (!fs.existsSync(blogPhotosPath)) fs.mkdirSync(blogPhotosPath);
-        post.photos.forEach((photo => {
-          const sourceUrl = photo.original_size.url;
-          const fileName = path.basename(sourceUrl);
-          const photoPath = path.join(blogPhotosPath, fileName);
-          request(sourceUrl).pipe(fs.createWriteStream(photoPath));
-        }));
+function downloadBlogPart(blogName: string, params: any): Observable<number> {
+  return blogPosts(blogName, 'photo', params).pipe(
+    map((response: any) => {
+      const blogPhotosPath = path.join('download', response.blog.name);
+      if (!fs.existsSync(blogPhotosPath)) fs.mkdirSync(blogPhotosPath);
+      response.posts.forEach((post => {
+        if (post.photos) {
+          post.photos.forEach((photo => {
+            const sourceUrl = photo.original_size.url;
+            const fileName = path.basename(sourceUrl);
+            const photoPath = path.join(blogPhotosPath, fileName);
+            request(sourceUrl).pipe(fs.createWriteStream(photoPath));
+          }));
+        }
+      }));
+      return response.posts.length;
+    }),
+    delay(4000),
+    switchMap((postsCount: number) => {
+      if (postsCount == params.limit) {
+        return downloadBlogPart(blogName, {limit: params.limit, offset: params.offset + params.limit});
       }
-    }));
-  }));
+      return of(postsCount);
+    })
+  );
 }
 
-downloadBlog('friendly-animals')
+function downloadBlog(blogName: string): Observable<any> {
+  const limit: number = 20;
+  let offset: number = 0;
+  return downloadBlogPart(blogName, {limit: limit, offset: offset});
+
+}
+
+downloadBlog('cutest-animals-here')
   .subscribe({
     next: () => {
       console.log('Job finished');
